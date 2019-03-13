48e509aa1ad6e5bf12ade98a5a793ff01c5365d0
Ivan Dudarev <ivan@ddrv.ru>
2019-03-13 15:41:53 +0700

    upgrade version

--- src/Mailer.php
+++ src/Mailer.php
@@ -10,7 +10,7 @@ use Ddrv\Mailer\Transport\TransportInterface;
 final class Mailer
 {
 
-    const MAILER_VERSION = "3.1.0";
+    const MAILER_VERSION = "3.1.1";
 
     const CHANNEL_DEFAULT = "default";
     const CHANNEL_ALL     = "*";
